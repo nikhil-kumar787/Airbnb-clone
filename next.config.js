c325531ff44229b129df091507520a1de9b18fe9
nikhil-kumar787 <nikhilkumar7167@gmail.com>
2021-08-07 14:35:57 +0530

    Added mapbox

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ["links.papareact.com"],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoic2FtbmlrNzg3IiwiYSI6ImNrczE1cGprdDAwM2Uyb3FtM2VsdjFubGsifQ.gYjdSa7lH3zxerOja819lQ'
     }
 }
